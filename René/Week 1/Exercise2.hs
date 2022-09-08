63b9b8c17418fae2622a5af8dc36a1fa30e2d543
René Kok <rkok98@gmail.com>
2022-09-08 14:52:54 +0200

    Finish exercise 2

--- René/Week 1/Exercise2.hs
+++ René/Week 1/Exercise2.hs
@@ -1,15 +1,26 @@
 import Data.List (subsequences)
-import Test.QuickCheck (quickCheck, Property, (==>))
+import Test.QuickCheck
+  ( Arbitrary (arbitrary),
+    Gen,
+    Property,
+    forAll,
+    quickCheck,
+    suchThat,
+    (==>),
+  )
 
 subsequencesLength :: Int -> Int
-subsequencesLength n = length $ subsequences [1..n]
+subsequencesLength n = length $ subsequences [1 .. n]
 
 subsequencesLength' :: Int -> Int
-subsequencesLength' n = (^n) 2
+subsequencesLength' n = (^ n) 2
 
 testTheorem4 :: Int -> Property
 testTheorem4 n = n >= 0 ==> subsequencesLength n == subsequencesLength' n
 
-exercise2 :: IO()
+genMax :: Gen Int
+genMax = (arbitrary :: Gen Int) `suchThat` (<= 25)
+
+exercise2 :: IO ()
 exercise2 = do
-    quickCheck testTheorem4
+  quickCheck $ forAll genMax testTheorem4
