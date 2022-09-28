56b8735ca1bfe43944e5cfd262000153aca14211
Donovan <donovan@sp-byods-145-109-13-136.wireless.uva.nl>
2022-09-28 15:54:03 +0200

    Added exercises to finalassignment

---
+++ FinalAssignment/W4/Exercise1.hs
@@ -0,0 +1,8 @@
+-- SSVT Haskell Lab
+-- Week 4 - Group 5
+-- Exercise 1: 
+-- Deliverables: 
+-- Time spend: ?? minutes --
+import Data.List
+import System.Random
+import Test.QuickCheck
---
+++ FinalAssignment/W4/Exercise4.hs
@@ -0,0 +1,8 @@
+-- SSVT Haskell Lab
+-- Week 4 - Group 5
+-- Exercise 4: 
+-- Deliverables: 
+-- Time spend: ?? minutes --
+import Data.List
+import System.Random
+import Test.QuickCheck
