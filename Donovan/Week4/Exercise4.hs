e09fab990de22a6d694e670b553cd2ac2c8d772e
Tim van Ekert <tim.van.ekert@hva.nl>
2022-09-29 11:37:42 +0200

    fix exercise 4

--- Donovan/Week4/Exercise4.hs
+++ Donovan/Week4/Exercise4.hs
@@ -1,14 +1,22 @@
 -- SSVT Haskell Lab
 -- Week 4 - Group 5
--- Exercise 1: The IOLTS datatype allows, by definition, for the creation of IOLTS's that are not valid. 
--- Make a list of factors that result in invalid IOLTS's. Write a function validateLTS :: IOLTS -> Bool that 
--- returns true iff a given LTS is valid according to the definition given in the Tretmans paper.
--- Deliverables: list of factors, implementation, short test report, indication of time spent.
+-- Exercise 4: Implement the function after (infix) for IOLTS corresponding with the definition in the Tretmans paper. 
+-- Create tests to test the validity of your implementation.
+-- Deliverables: Haskell program, tests, short test report, indication of time spent.
 -- Time spend: - hours --
 module Donovan.Week4.Exercise4 where
 import Data.List
 import Test.QuickCheck
 import Donovan.Week4.LTS
+
+returnNextState :: [LabeledTransition] -> [State] -> Trace -> [State]
+returnNextState transition startState trace
+    | null (trace) = startState
+    | not (null (trace)) = returnNextState transition ([nextState | (firstState, label, nextState) <- transition, firstState `elem` startState && label == head trace]) (tail trace)
+
+
+after :: IOLTS -> Trace -> [State]
+after (_, _, _, transition, startState) = returnNextState transition [startState]
 
 
 exercise4 :: IO ()
