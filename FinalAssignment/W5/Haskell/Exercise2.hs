37157789ca54048d25a16ebfc8047760eafb1c4e
René Kok <rkok98@gmail.com>
2022-10-05 18:24:01 +0200

    Exercise 2

--- FinalAssignment/W5/Haskell/Exercise2.hs
+++ FinalAssignment/W5/Haskell/Exercise2.hs
@@ -4,10 +4,30 @@
 -- Deliverables: implementation, documentation of approach, effect of using different mutators/properties, indication of time spent.
 -- Time spend: ~ hours --
 module FinalAssignment.W5.Haskell.Exercise2 where
-import FinalAssignment.W5.Haskell.Mutation
+
 import FinalAssignment.W5.Haskell.MultiplicationTable
+import FinalAssignment.W5.Haskell.Mutation
+import Test.QuickCheck
+import Data.Maybe
+
+allMutators = [addElements, removeElements]
+
+countSurvivors :: Int -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> [Gen [Maybe Bool]]
+countSurvivors nrOfMutants props fn =
+  [ vectorOf nrOfMutants (mutate mutator prop fn 3) | prop <- props, mutator <- allMutators]
 
 
+aa = do
+  x <- bb
+  let y =  length $ filter (\x -> x == Just False) (concat x)
+  return y
+
+-- count = tests >>= \x -> return $ length $ filter (== Just True) x
+bb :: Gen [[Maybe Bool]]
+bb = sequence $ countSurvivors 10 [prop_tenElements] multiplicationTable
+
+-- Voert alle props uit in combinatie met mutator
+-- Dan voeg je de lijsten samen door middel een lijst te maken van alle 1en
 
 exercise2 :: IO ()
 exercise2 = do
