0cc4d7a221cdb09735f70bb630757f1a14afbf5a
René Kok <rkok98@gmail.com>
2022-10-05 19:15:34 +0200

    Cleannnn

--- FinalAssignment/W5/Haskell/Exercise2.hs
+++ FinalAssignment/W5/Haskell/Exercise2.hs
@@ -2,20 +2,29 @@
 -- Week 5 - Group 5
 -- Exercise 2: Write a function countSurvivors that counts the number of survivors.
 -- Deliverables: implementation, documentation of approach, effect of using different mutators/properties, indication of time spent.
--- Time spend: ~ hours --
+-- Time spend: ~ 3 hours --
 module FinalAssignment.W5.Haskell.Exercise2 where
 
 import FinalAssignment.W5.Haskell.MultiplicationTable
 import FinalAssignment.W5.Haskell.Mutation
 import Test.QuickCheck
 
+type NrOfMutants = Int
+type Prop = [Integer] -> Integer -> Bool
+type Fn = (Integer -> [Integer])
+type Matrix = Gen [[Maybe Bool]]
+
+
+createMatrix :: NrOfMutants -> [Prop] -> Fn -> Matrix
+createMatrix nrOfMutants props fn = sequence $ [vectorOf nrOfMutants (mutate mutator prop fn 3) | prop <- props, mutator <- allMutators]
+
 allMutators :: [[Integer] -> Gen [Integer]]
 allMutators = [addElements, removeElements]
 
-countSurvivors :: Int -> [[Integer] -> Integer -> Bool] -> (Integer -> [Integer]) -> Gen Int
+countSurvivors :: NrOfMutants -> [Prop] -> Fn -> Gen Int
 countSurvivors nrOfMutants props fn = testsM >>= \tests -> return $ length $ filter (== Just True) $ concat tests
   where
-    testsM = sequence [vectorOf nrOfMutants (mutate mutator prop fn 3) | prop <- props, mutator <- allMutators]
+    testsM = createMatrix nrOfMutants props fn
 
 exercise2 :: IO ()
 exercise2 = do
