fec2e5b41a8ad447180f8cecfd05aa6e2485a1a7
Tim van Ekert <tim.van.ekert@hva.nl>
2022-10-05 11:30:55 +0200

    ex 1

---
+++ FinalAssignment/W5/Haskell/Exercise1.hs
@@ -0,0 +1,17 @@
+-- SSVT Haskell Lab
+-- Week 4 - Group 5
+-- Exercise 1: We provide some mutators to mutate the output of the list in Mutation.hs. Write down which types of output are not yet covered by these mutators, 
+-- and about their weakness/strength. Come up with a list of other mutators and implement (a subset of) them.
+-- Deliverables: List of mutators and rationale, implementation, indication of time spent.
+-- Time spend: ~ hours --
+module FinalAssignment.W5.Haskell.Exercise1 where
+import Data.List
+import Test.QuickCheck
+import FinalAssignment.W4.LTS
+
+
+
+exercise1 :: IO ()
+exercise1 = do
+  putStrLn "\bExercise 1\nTime spent +/- ~ hours\n"
+  putStrLn "\n"
