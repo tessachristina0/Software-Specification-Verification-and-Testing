d7cfab7bec95053d247744af526c7b1e93b992bc
René Kok <rkok98@gmail.com>
2022-10-05 22:17:13 +0200

    exercise3

--- FinalAssignment/W5/Haskell/Exercise3.hs
+++ FinalAssignment/W5/Haskell/Exercise3.hs
@@ -6,8 +6,32 @@
 module FinalAssignment.W5.Haskell.Exercise3 where
 import FinalAssignment.W5.Haskell.Mutation
 import FinalAssignment.W5.Haskell.MultiplicationTable
+import FinalAssignment.W5.Haskell.Exercise2
+import Test.QuickCheck
 
-createMatrix :: 
+-- Conjuctie, Disjunctie en equiv
+
+-- Stap 1: Per property ga je elke mutator af
+-- Stap 2: Alle resultaten checken
+
+
+-- Gen [[Maybe Bool]]
+
+
+-- createMatrix' _ [] _ _ = []
+createMatrix' nrOfMutants props mutators fn = aa >>= \a -> return (concat a) -- ++ createMatrix' nrOfMutants (tail props) mutators fn
+  where aa = sequence $ [vectorOf nrOfMutants (mutate mut (head props) fn 3) | mut <- mutators]
+
+
+ee nrOfMutants props mutators fn = concatMap (\prop -> concatMap (\mut -> [vectorOf nrOfMutants (mutate mut prop fn 3) ]) mutators) props
+
+ff nrOfMutants props mutators fn = concatMap x props
+  where 
+    x = \prop -> [vectorOf nrOfMutants (mutate mut prop fn 3) | mut <- mutators]
+--    y = if prop == 
+  
+
+dd = generate $ sequence $ ee 1 [prop_tenElements, prop_firstElementIsInput, prop_sumIsTriangleNumberTimesInput] [addElements, removeElements] multiplicationTable
 
 exercise3 :: IO ()
 exercise3 = do
