014e1c75f2de398f99bf1b4f1032c31c69338095
René Kok <rkok98@gmail.com>
2022-10-12 12:39:32 +0200

    Initial W6

---
+++ FinalAssignment/W6/Exercise1.hs
@@ -0,0 +1,9 @@
+-- SSVT Haskell Lab
+-- Week 6 - Group 5
+-- Exercise 1: 
+-- Time spend: ~ hours --
+module FinalAssignment.W6.Exercise1 where
+import FinalAssignment.W6.SetOrd
+import Test.QuickCheck
+import Data.List
+
