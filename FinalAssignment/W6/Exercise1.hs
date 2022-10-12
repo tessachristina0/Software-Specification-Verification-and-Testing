35aee34f3354faa6bb01e136916a694ca7fd36b2
Tim van Ekert <tim.van.ekert@hva.nl>
2022-10-12 14:23:40 +0200

    Add random set generation

--- FinalAssignment/W6/Exercise1.hs
+++ FinalAssignment/W6/Exercise1.hs
@@ -1,9 +1,40 @@
 -- SSVT Haskell Lab
 -- Week 6 - Group 5
--- Exercise 1: 
--- Time spend: ~ hours --
+-- Exercise 1: Implement a random data generator for the datatype Set Int, where Set is as defined in SetOrd.hs. First do this from scratch, next give a version that uses QuickCheck to random test this datatype.
+-- Deliverables: two random test generators, indication of time spent.
+-- Time spend: 1 hour --
 module FinalAssignment.W6.Exercise1 where
 import FinalAssignment.W6.SetOrd
-import Test.QuickCheck
 import Data.List
+import System.Random
+import Test.QuickCheck
+import Control.Monad
 
+-- Function to create a random list
+randomList :: Int -> Int -> Int -> IO [Int]
+randomList num x y = replicateM num (randomRIO (x,y::Int))
+
+-- Function to translate a random list into a set
+randomSet :: Int -> Int -> Int -> IO (Set Int)
+randomSet num x y = do
+    list <- randomList num x y
+    return (list2set list)
+
+-- Function to create a random list using the quickcheck generate with choose, which generates a random element in a given range
+randomListQuickCheck :: Int -> Int -> Int -> IO [Int]
+randomListQuickCheck num x y = replicateM num (generate $ choose(x, y))
+
+-- Function to translate a random list into a set but then for the quickcheck function
+randomSetQuickCheck :: Int -> Int -> Int -> IO (Set Int)
+randomSetQuickCheck num x y = do
+    list <- randomListQuickCheck num x y
+    return (list2set list)
+
+exercise1 :: IO ()
+exercise1 = do
+    putStrLn "\bExercise 1\nTime spent +/- 1 hour\n"
+    putStrLn "Random Set: "
+    randomSet 20 0 100
+    putStrLn "Random Set using QuickCheck: "
+    randomSetQuickCheck 20 0 100
+    putStrLn ""
