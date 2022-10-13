679f8a5227592321a39ca7a0df4a474d228d1fab
René Kok <rkok98@gmail.com>
2022-10-13 17:44:35 +0200

    Finalized exercise 6

--- FinalAssignment/W6/Exercise6.hs
+++ FinalAssignment/W6/Exercise6.hs
@@ -15,25 +15,26 @@ import Test.QuickCheck
 
 type Rel a = [(a, a)]
 
-newtype Int' = Int' Int deriving (Show, Eq, Ord)
+-- Restrict the items to be between 1 and 10
+newtype Item = Item Int deriving (Show, Eq, Ord)
 
-instance Arbitrary Int' where
-  arbitrary :: Gen Int'
-  arbitrary = oneof $ map (return . Int') [1 .. 8]
+instance Arbitrary Item where
+  arbitrary :: Gen Item
+  arbitrary = oneof [return (Item n) | n <- [1 .. 10]]
 
 -- TRANSITIVE CLOSURE OF A RELATIONSHIP
 isTransitive :: Eq a => Rel a -> Bool
-isTransitive rel = and [ (a1, b2) `elem` rel | (a1, a2) <- rel, 
-                                               (b1, b2) <- rel, 
+isTransitive rel = and [ (a1, b2) `elem` rel | (a1, a2) <- rel,
+                                               (b1, b2) <- rel,
                                                a2 == b1 ]
 
 -- All relations should be transitive
-prop_relationsAreTransitive :: Rel Int' -> Bool
+prop_relationsAreTransitive :: Rel Item -> Bool
 prop_relationsAreTransitive rel = isTransitive $ trClos rel
 
 -- The intersection of two transitive relations is transitive.
 -- See https://en.wikipedia.org/wiki/Transitive_closure#Properties
-prop_intrsectIsTransitive :: Rel Int' -> Rel Int' -> Bool
+prop_intrsectIsTransitive :: Rel Item -> Rel Item -> Bool
 prop_intrsectIsTransitive rel1 rel2 = isTransitive $ trClos (rel1 `intersect` rel2)
 
 -- The union of two transitive relations need not be transitive.
@@ -42,11 +43,11 @@ prop_intrsectIsTransitive rel1 rel2 = isTransitive $ trClos (rel1 `intersect` re
 -- To obtain a new equivalence relation take the transitive closure
 -- (reflexivity and symmetry—in the case of equivalence relations—are automatic)
 -- See https://en.wikipedia.org/wiki/Transitive_closure#Properties
-prop_unionIsNotTransitive :: Rel Int' -> Rel Int' -> Bool
+prop_unionIsNotTransitive :: Rel Item -> Rel Item -> Bool
 prop_unionIsNotTransitive rel1 rel2 = isTransitive $ trClos (trClos rel1 `union` trClos rel2)
 
 -- Transitive Closure should be sorted
-prop_trClosIsSorted :: Rel Int' -> Bool
+prop_trClosIsSorted :: Rel Item -> Bool
 prop_trClosIsSorted rel = sort trClos' == trClos'
   where
     trClos' = trClos rel
@@ -56,11 +57,11 @@ isSymmetric :: Eq a => Rel a -> Bool
 isSymmetric rel = and [(b, a) `elem` rel | (a, b) <- rel]
 
 -- All relations should be symmetric
-prop_relationsAreSymmetric :: Rel Int' -> Bool
+prop_relationsAreSymmetric :: Rel Item -> Bool
 prop_relationsAreSymmetric rel = isSymmetric $ symClos rel
 
 -- Symmetric closure should be sorted
-prop_symClosIsSorted :: Rel Int' -> Bool
+prop_symClosIsSorted :: Rel Item -> Bool
 prop_symClosIsSorted rel = sort symClos' == symClos'
   where
     symClos' = symClos rel
