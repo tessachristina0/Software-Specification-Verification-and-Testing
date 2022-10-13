c2841496dc8fb661214e38b9942ac32d7e0d3e5b
Donovan <donovan@donovans-mbp.kpn>
2022-10-13 11:34:06 +0200

    Exercise2 WIP

--- FinalAssignment/W6/Exercise2.hs
+++ FinalAssignment/W6/Exercise2.hs
@@ -7,11 +7,6 @@ import FinalAssignment.W6.SetOrd
 import Test.QuickCheck
 import Data.List
 import FinalAssignment.W6.Exercise1
-import Test.QuickCheck (Arbitrary)
-
--- instance Arbitrary (Set a) where
---   arbitrary :: Gen (Set a)
---   arbitrary = oneof (map (return (Set a)))
 
 -- Implementations
 setIntersection :: Ord a => Set a -> Set a -> Set a
@@ -29,59 +24,54 @@ setDifference (Set set1) (Set set2) =  Set(set1 \\ set2)
 
 -- Testing using custom generator
 prop_1 :: IO Bool
-prop_1 = do 
-    (Set set1) <- randomSetQuickCheck 10 0 100
-    (Set set2) <- randomSetQuickCheck 10 0 100
+prop_1 = do
+    (Set set1) <- randomSet 10 0 100
+    (Set set2) <- randomSet 10 0 100
     return (setIntersection (Set set1) (Set set2) == Set (set1 `intersect` set2))
 
 prop_1' :: IO Bool
-prop_1' = do 
-    (Set set1) <- randomSet 10 0 100
-    (Set set2) <- randomSet 10 0 100
+prop_1' = do
+    (Set set1) <- randomSetQuickCheck 10 0 100
+    (Set set2) <- randomSetQuickCheck 10 0 100
     return (setIntersection (Set set1) (Set set2) == Set (set1 `intersect` set2))
 
 prop_2 :: IO Bool
-prop_2 = do 
-    (Set set1) <- randomSetQuickCheck 10 0 100
-    (Set set2) <- randomSetQuickCheck 10 0 100
+prop_2 = do
+    (Set set1) <- randomSet 10 0 100
+    (Set set2) <- randomSet 10 0 100
     return (setUnion (Set set1) (Set set2) == unionSet (Set set1) (Set set2))
 
 prop_2' :: IO Bool
-prop_2' = do 
-    (Set set1) <- randomSet 10 0 100
-    (Set set2) <- randomSet 10 0 100
+prop_2' = do
+    (Set set1) <- randomSetQuickCheck 10 0 100
+    (Set set2) <- randomSetQuickCheck 10 0 100
     return (setUnion (Set set1) (Set set2) == unionSet (Set set1) (Set set2))
 
 prop_3 :: IO Bool
-prop_3 = do 
-    (Set set1) <- randomSetQuickCheck 10 0 100
-    (Set set2) <- randomSetQuickCheck 10 0 100
-    return (setDifference (Set set1) (Set set2) == Set (set1 \\ set2))
-
-prop_3' :: IO Bool
-prop_3' = do 
+prop_3 = do
     (Set set1) <- randomSet 10 0 100
     (Set set2) <- randomSet 10 0 100
     return (setDifference (Set set1) (Set set2) == Set (set1 \\ set2))
 
--- Testing using QuickCheck
-prop_1_qc :: Ord a => Set a -> Set a -> Bool
-prop_1_qc (Set set1) (Set set2) = setIntersection (Set set1) (Set set2) == Set (set1 `intersect` set2)
-
-prop_2_qc :: Ord a => Set a -> Set a -> Bool
-prop_2_qc (Set set1) (Set set2) = setUnion (Set set1) (Set set2) == unionSet (Set set1) (Set set2)
-
-prop_3_qc :: Ord a => Set a -> Set a -> Bool
-prop_3_qc (Set set1) (Set set2) = setDifference (Set set1) (Set set2) == Set (set1 \\ set2)
+prop_3' :: IO Bool
+prop_3' = do
+    (Set set1) <- randomSetQuickCheck 10 0 100
+    (Set set2) <- randomSetQuickCheck 10 0 100
+    return (setDifference (Set set1) (Set set2) == Set (set1 \\ set2))
 
 exercise2 :: IO ()
 exercise2 = do
     putStrLn "\bExercise 2\nTime spent +/- 1 hour\n"
     putStrLn "setIntersection test without quickCheck"
-    randomSet 20 0 100
-
+    prop_1
     putStrLn "setIntersection test with quickCheck"
-    quickCheck prop_1_qc
-    putStrLn "Random Set using QuickCheck: "
-    randomSetQuickCheck 20 0 100
+    prop_1'
+    putStrLn "setUnion test without quickCheck"
+    prop_2
+    putStrLn "setUnion test with quickCheck"
+    prop_2'
+    putStrLn "setDifference test without quickCheck"
+    prop_3
+    putStrLn "setDifference test with quickCheck"
+    prop_3'
     putStrLn ""
